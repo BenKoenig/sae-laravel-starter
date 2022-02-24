8ca9888aafabec5fc894c828fbd3d9aea4d1a0f1
Benjamin König <bkoenig.as@gmail.com>
2022-02-24 13:57:07 +0100

    Initial commit

---
+++ resources/vue/HelloWorld/HelloWorld.vue
@@ -0,0 +1,62 @@
+
+<script>
+
+import axios from 'axios'
+
+export default {
+
+  data() {
+
+    return {
+
+      postings: [],
+    }
+  },
+
+  created() {
+
+    console.info('Hello World')
+
+    this.loadPostings()
+  },
+
+  computed: {
+
+
+  },
+
+  methods: {
+
+    loadPostings() {
+
+      axios.get('/postings').then(response => {
+
+        this.postings = response.data.data
+      })
+    },
+
+  },
+}
+
+</script>
+
+
+<template>
+
+  <h1 class="text-bold text-4xl pt-[10rem]">Hello World!!!</h1>
+
+  <p v-for="posting in postings">
+    {{ posting.title }}
+  </p>
+
+</template>
+
+
+<style>
+
+  h1 {
+
+    color: red;
+  }
+
+</style>
---
+++ webpack.mix.js
@@ -0,0 +1,35 @@
+const mix = require('laravel-mix');
+const tailwindcss = require('tailwindcss');
+
+
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel applications. By default, we are compiling the CSS
+ | file for the application as well as bundling up all the JS files.
+ |
+ */
+
+/*mix.sass('resources/sass/app.scss', 'public/css').version()*/
+
+mix.js('resources/vue/HelloWorld', 'public/js/vue-app.js').vue().version()
+
+mix.js('resources/vue/HelloWorld/index.js', 'public/js')
+    .sass('resources/sass/app.scss', 'public/css')
+    .options({
+        postCss: [ tailwindcss('./tailwind.config.js') ],
+    })
+    .version();
+
+
+
+/*mix.scripts([
+
+    'node_modules/bootstrap/dist/js/bootstrap.bundle.js',
+
+], 'public/js/app.js').version()*/
+
+mix.copy('node_modules/font-awesome/fonts', 'public/fonts')
